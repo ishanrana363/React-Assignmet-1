7d470dc932feee545d0136c1950e3ec7f38749b9
mernishan <ishanrana500510@gmail.com>
2023-10-28 20:59:23 +0600

    assignment done

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -1,11 +1,34 @@
-import datas from "../data.json"
+import datas from "../data.json";
+import "../css/style.css"
 const Product = () => {
     return (
-        <div>
+        <div className="product" >
             {
                 datas.map((data,index)=>{
                     return(
-                        <div key={index} className="card" >
+                        <div key={index} className="card-details" >
+                            <img src={data.image} alt="" />
+                            <h3 className="cardTilte" >
+                                {data.title}
+                            </h3>
+                            <p className="productPrice" >
+                                Price : $ {
+                                    data.price
+                                }
+                            </p>
+                            <p className="productRating" >
+                                Rating : {
+                                    data.rating.rate
+                                }/5
+                            </p>
+                            <p className="productDescription" >
+                                Description:{
+                                    data.description
+                                }
+                            </p>
+                            <button className="productButton" >
+                                Add to cart
+                            </button>
                         </div>
                     )
                 })
